64ec636747914e00fbf2994b08500e84a8d697f9
root <root@ip-172-31-85-206.ec2.internal>
2023-01-28 09:59:52 +0000

    머가바낌?

--- back/config/express.js
+++ back/config/express.js
@@ -13,7 +13,7 @@ module.exports = function () {
   app.use(express.urlencoded({ extended: true })); //form 으로 제출되는 값 파싱
   app.use(methodOverride()); // put, delete 요청 처리
   app.use(cors()); // 웹브라우저 cors 설정을 관리
-  app.use(express.static("/home/ubuntu/food-map/front")); 
+  app.use(express.static("/home/ubuntu/FoodMap/front")); 
 
   // app.use(express.static("/home/ubuntu/food-map/front"))
   // app.use(express.static(process.cwd() + '/public'));

8579d06071c811fa3d313c6d76a4727dae3f5655
memilmooki <amelius4@naver.com>
2023-02-05 17:28:38 +0900

    login

--- back/src/routes/indexRoute.js
+++ back/src/routes/indexRoute.js
@@ -9,4 +9,12 @@ module.exports = function (app) {
   //식당 목록 조회
   app.get("/Restaurants", index.readRestaurants);
 
+  //회원가입
+  app.post("/sing-up", index.createUsers);
+
+  //로그인
+  app.post("/sign-in", index.createJwt);
+
+  //로그인 유지, 토큰 검증
+  app.get("/jwt", jwtMiddleware, index.readJwt);
 };
